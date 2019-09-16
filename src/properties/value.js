1cf65be3d409c48c9a8bae16224132a02b19c937
Wil Wilsman <wil@wilwilsman.com>
2019-09-15 21:37:23 -0500

    :sparkles: Allow regex matchers for value assertions (#20)

--- src/properties/value.js
+++ src/properties/value.js
@@ -1,16 +1,15 @@
 import computed from '../helpers/computed';
+import { q, eq } from '../utils/string';
 
 export default function value(selector) {
   return computed(
     selector,
     element => element.value,
     (actual, expected) => ({
-      result: actual === expected,
-      message: () => (
-        actual === expected
-          ? `%s value is "${expected}"`
-          : `%s value is "${actual}" but expected "${expected}"`
-      )
+      result: eq(actual, expected),
+      message: result => result
+        ? `%s value is ${q(expected)}`
+        : `%s value is ${q(actual)} but expected ${q(expected)}`
     })
   );
 }
