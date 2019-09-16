b555a2c609215a2360b2b140af79b7c710c72d6d
Wil Wilsman <wil@wilwilsman.com>
2019-09-15 21:37:14 -0500

    :sparkles: Allow regex matchers for text assertions (#20)

--- src/properties/text.js
+++ src/properties/text.js
@@ -1,16 +1,15 @@
 import computed from '../helpers/computed';
+import { q, eq } from '../utils/string';
 
 export default function text(selector) {
   return computed(
     selector,
     element => element.innerText,
     (actual, expected) => ({
-      result: actual === expected,
-      message: () => (
-        actual === expected
-          ? `%s text is "${expected}"`
-          : `%s text is "${actual}" but expected "${expected}"`
-      )
+      result: eq(actual, expected),
+      message: result => result
+        ? `%s text is ${q(expected)}`
+        : `%s text is ${q(actual)} but expected ${q(expected)}`
     })
   );
 }
