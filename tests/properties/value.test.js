a17c5c8d2138914b7319a3e4909f1932b21266cf
Wil Wilsman <wil@wilwilsman.com>
2019-09-15 21:37:23 -0500

    :sparkles: Allow regex matchers for value assertions (#20)

--- tests/properties/value.test.js
+++ tests/properties/value.test.js
@@ -24,26 +24,38 @@ describe('Interactor properties - value', () => {
 
       it('resolves when passing', async () => {
         await expect(input.assert.value('hello world')).resolves.toBeUndefined();
+        await expect(input.assert.value(/world/)).resolves.toBeUndefined();
         await expect(input.assert.not.value('HELLO')).resolves.toBeUndefined();
+        await expect(input.assert.not.value(/hi/)).resolves.toBeUndefined();
       });
 
       it('resolves when passing with a selector', async () => {
         await expect(field.assert.value('input', 'hello world')).resolves.toBeUndefined();
+        await expect(field.assert.value('input', /w.r.d/)).resolves.toBeUndefined();
         await expect(field.assert.not.value('input', 'HELLO')).resolves.toBeUndefined();
+        await expect(field.assert.not.value('input', /h{2}/)).resolves.toBeUndefined();
       });
 
       it('rejects with an error when failing', async () => {
         await expect(input.assert.value('hallo worldo'))
           .rejects.toThrow('value is "hello world" but expected "hallo worldo"');
+        await expect(input.assert.value(/hi/i))
+          .rejects.toThrow('value is "hello world" but expected /hi/i');
         await expect(input.assert.not.value('hello world'))
           .rejects.toThrow('value is "hello world"');
+        await expect(input.assert.not.value(/world/))
+          .rejects.toThrow('value is /world/');
       });
 
       it('rejects with an error when failing with a selector', async () => {
         await expect(field.assert.value('input', 'hallo worldo'))
           .rejects.toThrow('value is "hello world" but expected "hallo worldo"');
+        await expect(field.assert.value('input', /WORLD/))
+          .rejects.toThrow('value is "hello world" but expected /WORLD/');
         await expect(field.assert.not.value('input', 'hello world'))
           .rejects.toThrow('value is "hello world"');
+        await expect(field.assert.not.value('input', /l{2}/))
+          .rejects.toThrow('value is /l{2}/');
       });
     });
   });
@@ -64,14 +76,20 @@ describe('Interactor properties - value', () => {
     describe('and the default assertion', () => {
       it('resolves when passing', async () => {
         await expect(field.assert.value('hello world')).resolves.toBeUndefined();
+        await expect(field.assert.value(/world/)).resolves.toBeUndefined();
         await expect(field.assert.not.value('HELLO')).resolves.toBeUndefined();
+        await expect(field.assert.not.value(/WORLD/)).resolves.toBeUndefined();
       });
 
       it('rejects with an error when failing', async () => {
         await expect(field.assert.value('hallo worldo'))
           .rejects.toThrow('value is "hello world" but expected "hallo worldo"');
+        await expect(field.assert.value(/hallo/))
+          .rejects.toThrow('value is "hello world" but expected /hallo/');
         await expect(field.assert.not.value('hello world'))
           .rejects.toThrow('value is "hello world"');
+        await expect(field.assert.not.value(/h.llo/))
+          .rejects.toThrow('value is /h.llo/');
       });
     });
 
@@ -82,14 +100,20 @@ describe('Interactor properties - value', () => {
 
       it('resolves when passing', async () => {
         await expect(field.assert.foo('hello world')).resolves.toBeUndefined();
+        await expect(field.assert.foo(/w.r.d/)).resolves.toBeUndefined();
         await expect(field.assert.not.foo('HELLO')).resolves.toBeUndefined();
+        await expect(field.assert.not.foo(/h{2}/)).resolves.toBeUndefined();
       });
 
       it('rejects with an error when failing', async () => {
         await expect(field.assert.foo('hallo worldo'))
           .rejects.toThrow('value is "hello world" but expected "hallo worldo"');
+        await expect(field.assert.foo(/ha/))
+          .rejects.toThrow('value is "hello world" but expected /ha/');
         await expect(field.assert.not.foo('hello world'))
           .rejects.toThrow('value is "hello world"');
+        await expect(field.assert.not.foo(/h.l{2}o/))
+          .rejects.toThrow('value is /h.l{2}o/');
       });
     });
   });
