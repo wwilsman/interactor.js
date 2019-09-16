257ad32334e5bb5a018936f6fe7ca1df6bd6c8ac
Wil Wilsman <wil@wilwilsman.com>
2019-09-15 21:37:14 -0500

    :sparkles: Allow regex matchers for text assertions (#20)

--- tests/properties/text.test.js
+++ tests/properties/text.test.js
@@ -22,26 +22,38 @@ describe('Interactor properties - text', () => {
     describe('and the default assertion', () => {
       it('resolves when passing', async () => {
         await expect(p.assert.text('Hello WORLD!')).resolves.toBeUndefined();
+        await expect(p.assert.text(/world/i)).resolves.toBeUndefined();
         await expect(p.assert.not.text('WORLD')).resolves.toBeUndefined();
+        await expect(p.assert.not.text(/wild/)).resolves.toBeUndefined();
       });
 
       it('resolves when passing with a selector', async () => {
         await expect(p.assert.text('span', 'WORLD')).resolves.toBeUndefined();
+        await expect(p.assert.text('span', /wo/i)).resolves.toBeUndefined();
         await expect(p.assert.not.text('span', 'Hello WORLD!')).resolves.toBeUndefined();
+        await expect(p.assert.not.text('span', /hello/i)).resolves.toBeUndefined();
       });
 
       it('rejects with an error when failing', async () => {
         await expect(p.assert.text('hello world'))
           .rejects.toThrow('text is "Hello WORLD!" but expected "hello world"');
+        await expect(p.assert.text(/world/))
+          .rejects.toThrow('text is "Hello WORLD!" but expected /world/');
         await expect(p.assert.not.text('Hello WORLD!'))
           .rejects.toThrow('text is "Hello WORLD!"');
+        await expect(p.assert.not.text(/world/i))
+          .rejects.toThrow('text is /world/i');
       });
 
       it('rejects with an error when failing with a selector', async () => {
         await expect(p.assert.text('span', 'world'))
           .rejects.toThrow('text is "WORLD" but expected "world"');
+        await expect(p.assert.text('span', /world/))
+          .rejects.toThrow('text is "WORLD" but expected /world/');
         await expect(p.assert.not.text('span', 'WORLD'))
           .rejects.toThrow('text is "WORLD"');
+        await expect(p.assert.not.text('span', /world/i))
+          .rejects.toThrow('text is /world/i');
       });
     });
   });
@@ -62,14 +74,20 @@ describe('Interactor properties - text', () => {
     describe('and the default assertion', () => {
       it('resolves when passing', async () => {
         await expect(p.assert.text('WORLD')).resolves.toBeUndefined();
+        await expect(p.assert.text(/world/i)).resolves.toBeUndefined();
         await expect(p.assert.not.text('Hello')).resolves.toBeUndefined();
+        await expect(p.assert.not.text(/wo/)).resolves.toBeUndefined();
       });
 
       it('rejects with an error when failing', async () => {
         await expect(p.assert.text('Hello WORLD!'))
           .rejects.toThrow('text is "WORLD" but expected "Hello WORLD!"');
+        await expect(p.assert.text(/hello/i))
+          .rejects.toThrow('text is "WORLD" but expected /hello/i');
         await expect(p.assert.not.text('WORLD'))
           .rejects.toThrow('text is "WORLD"');
+        await expect(p.assert.not.text(/world/i))
+          .rejects.toThrow('text is /world/i');
       });
     });
 
@@ -80,14 +98,20 @@ describe('Interactor properties - text', () => {
 
       it('resolves when passing', async () => {
         await expect(p.assert.foo('WORLD')).resolves.toBeUndefined();
+        await expect(p.assert.foo(/world/i)).resolves.toBeUndefined();
         await expect(p.assert.not.foo('Hello')).resolves.toBeUndefined();
+        await expect(p.assert.not.foo(/wo/)).resolves.toBeUndefined();
       });
 
       it('rejects with an error when failing', async () => {
         await expect(p.assert.foo('Hello WORLD!'))
           .rejects.toThrow('text is "WORLD" but expected "Hello WORLD!"');
+        await expect(p.assert.text(/hello/i))
+          .rejects.toThrow('text is "WORLD" but expected /hello/i');
         await expect(p.assert.not.foo('WORLD'))
           .rejects.toThrow('text is "WORLD"');
+        await expect(p.assert.not.text(/world/i))
+          .rejects.toThrow('text is /world/i');
       });
     });
   });
