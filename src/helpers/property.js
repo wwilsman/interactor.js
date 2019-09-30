0bfb6f987b336bd4d0f38b23a7dd7f13878f7b84
Wil Wilsman <wil@wilwilsman.com>
2019-09-29 19:26:18 -0500

    :sparkles: Show layout engine warning for specific DOM properties

--- src/helpers/property.js
+++ src/helpers/property.js
@@ -1,8 +1,26 @@
+import { hasLayoutEngine } from '../utils/dom';
+
+const LAYOUT_PROPS = [
+  'scrollTop',
+  'scrollLeft',
+  'scrollWidth',
+  'scrollHeight',
+  'clientTop',
+  'clientLeft',
+  'clientWidth',
+  'clientHeight'
+];
+
 export function args(a) {
   return a.length <= 1 ? [undefined, a[0]] : a;
 }
 
 export default function property() {
   let [selector, prop] = args(arguments);
+
+  if (LAYOUT_PROPS.includes(prop)) {
+    hasLayoutEngine.call(this, `Properties effected by the result of CSS cannot be calculated (${prop})`);
+  }
+
   return this.$(selector)[prop];
 }
