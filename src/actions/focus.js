2ab510923539c698487a2212662772a384e0e2eb
Wil Wilsman <wil@wilwilsman.com>
2019-09-29 19:22:35 -0500

    :bug: Fix issue where hasFocus can incorrectly return false in jsdom

--- src/actions/focus.js
+++ src/actions/focus.js
@@ -3,7 +3,14 @@ import scoped from '../helpers/scoped';
 /* istanbul ignore next: the document always has focus during tests so this
  * function is never encountered */
 function isFramed({ self, top, frameElement }) {
-  return self !== top && frameElement;
+  return self !== top && !!frameElement;
+}
+
+/* istanbul ignore next: the document always has focus during tests so the
+ * second half of the condition below is never encountered */
+function hasFocus({ document }) {
+  // in some environments (jsdom), hasFocus can return false incorrectly
+  return document.hasFocus() || document.body === document.activeElement;
 }
 
 export default function focus(selector) {
@@ -13,15 +20,15 @@ export default function focus(selector) {
     .assert.f('Failed to focus %s: %e')
   // invoke the native DOM method
     .do(function(element) {
-      // if no document focus and in an iframe, try to steal focus
+      // if in an iframe, try to steal focus
       /* istanbul ignore next: document always has focus during tests */
-      if (!this.$dom.document.hasFocus() && isFramed(this.$dom)) {
+      if (!hasFocus(this.$dom) && isFramed(this.$dom)) {
         this.$dom.frameElement.focus();
       }
 
-      // document does not have focus
-      /* istanbul ignore if: document always has focus during tests */
-      if (!this.$dom.document.hasFocus()) {
+      // if the document cannot be focused, the element cannot recieve focus
+      /* istanbul ignore next: document always has focus during tests */
+      if (!hasFocus(this.$dom)) {
         throw new Error('the document is not focusable');
       }
 
