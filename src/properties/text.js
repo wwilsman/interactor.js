db877445bf8a5ae90f192b9fa573268b7246d9fd
Wil Wilsman <wil@wilwilsman.com>
2019-09-29 19:35:13 -0500

    :sparkles: Add layout engine warning to text property

--- src/properties/text.js
+++ src/properties/text.js
@@ -1,10 +1,18 @@
 import computed from '../helpers/computed';
+import { hasLayoutEngine } from '../utils/dom';
 import { q, eq } from '../utils/string';
 
 export default function text(selector) {
   return computed(
     selector,
-    element => element.innerText,
+    function(element) {
+      return hasLayoutEngine.call(this, (
+        'The appearance of text as a result of CSS cannot be determined ' +
+          '(such as pseudo content and text transformations)'
+      ))
+        ? element.innerText
+        : element.textContent;
+    },
     (actual, expected) => ({
       result: eq(actual, expected),
       message: result => result
