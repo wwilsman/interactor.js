46c5502efc98c20296e9cd07a171a28d356c0224
Wil Wilsman <wil@wilwilsman.com>
2020-01-27 12:25:00 -0600

    :sparkles::camera_flash: Add percy gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,9 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-percy'
+    },
+    {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID
