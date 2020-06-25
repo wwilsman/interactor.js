144cab1cb68b1fadfd65fe475ccbd24efbd3a6e6
Wil Wilsman <wil@wilwilsman.com>
2020-06-25 13:31:03 -0500

    ✨ Better negate assertions

--- src/assert.js
+++ src/assert.js
@@ -3,39 +3,61 @@ import error from './error';
 
 import {
   assign,
+  create,
   defineProperty,
   defineProperties,
   mapPropertyDescriptors
 } from './utils';
 
-// Builds a assert context around assertion functions and child interactors. This context is used
-// for named asserts and allows reusing existing and shared asseriton functions.
+// Negate assertions, bind the assert context, and handle returned interactor errors.
+function wrap(ctx, fn, expected, boundargs = []) {
+  return function(...args) {
+    let ret;
+
+    // create a context when necessary
+    ctx = ctx || context(this, expected);
+
+    try {
+      ret = fn.apply(ctx, boundargs.concat(args));
+    } catch (e) {
+      // rethrow or swallow when negated
+      if (expected) throw e;
+      return;
+    }
+
+    // handle interactor errors
+    if (ret?.name === 'InteractorError' && ret.result !== expected) {
+      throw ret.bind(this);
+    }
+
+    if (!expected) {
+      throw error('expected assertion to fail but it passed');
+    }
+  };
+}
+
+// Builds an assert context around assertion functions and child interactors. This context is used
+// for named asserts and allows reusing existing and shared assertion functions.
 function context(inst, expected) {
   let { fns, children } = m.get(inst.constructor.prototype.assert);
+  let ctx = create(null);
 
-  let ctx = defineProperties(
+  defineProperties(ctx, assign(
     // bind all custom assertions to the context
-    mapPropertyDescriptors(fns, ({ value: fn }) => function() {
-      try {
-        fn.apply(ctx, [expected].concat(arguments));
-        // ensure interactor errors retain the right context before bubbling
-        if (error.name === 'InteractorError' && !error.ctx) {
-          throw assign(error, { ctx: inst });
-        } else {
-          throw error;
-        }
-      }
-    }),
+    mapPropertyDescriptors(fns, ({ value: fn }) => ({
+      value: wrap(ctx, fn, expected)
+    })),
     // child interactors create assert context references
-    assign(mapPropertyDescriptors(children, ({ value }) => ({
-      get: () => context(m.new(value, 'parent', inst))
+    mapPropertyDescriptors(children, ({ value }) => ({
+      get: () => context(m.new(value, 'parent', inst), expected)
     })), {
       // allow assertions access to the interactor element
-      $: { value: inst.$.bind(inst) }
-    })
-  );
+      $: { value: inst.$.bind(inst) },
+      $$: { value: inst.$$.bind(inst) }
+    }
+  ));
 
-  if (expected) {
+  if (expected || m.get(inst, 'parent')) {
     // lazily create a negated context
     defineProperty(ctx, 'not', {
       get: () => context(inst, false)
@@ -49,16 +71,18 @@ function context(inst, expected) {
 // functions, child assertions, and other core assert methods.
 export default function InteractorAssert(inst, expected = true) {
   let passert = inst.constructor.prototype.assert;
-  let { fns = {}, children = {} } = m.get(passert) || {};
+  let { fns, children } = m.get(passert);
 
-  let assert = defineProperties(function() {
-    return passert.apply(this, arguments);
+  let assert = defineProperties(function assert(fn) {
+    // generic assertions are called with an assert context
+    return passert.call(inst, defineProperties(wrap(null, fn, expected), {
+      // used to determine invoking the interactor element
+      length: { value: fn.length }
+    }));
   }, assign(
-    // assertion functions are bound to an assert context and the current expected result
+    // assertion functions are bound to an assert context
     mapPropertyDescriptors(fns, ({ value: fn }) => ({
-      value: (...args) => passert.call(inst, function() {
-        fn.apply(context(this, expected), [expected].concat(args));
-      })
+      value: (...args) => passert.call(inst, wrap(null, fn, expected, args))
     })),
     // child interactors create child assert references
     mapPropertyDescriptors(children, ({ value: i }) => ({
