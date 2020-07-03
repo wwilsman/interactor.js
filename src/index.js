7310d968e1c4c0dd8197b496f2f9636637334ecf
Wil Wilsman <wil@wilwilsman.com>
2020-07-03 10:40:13 -0500

    ✨ Export generated action creators

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+import m from './meta';
+import { assign, map } from './utils';
+
+import Interactor from './interactor';
+import * as actions from './actions';
+
+assign(exports, map(actions, action => {
+  return function(...args) {
+    return action.apply((
+      m.get(this, 'queue') ? this
+        : Interactor(args.shift())
+    ), args);
+  };
+}));
+
 export * from './selectors';
 export { default as when } from './when';
 export { default as InteractorError } from './error';
-export { default } from './interactor';
+export default Interactor;
