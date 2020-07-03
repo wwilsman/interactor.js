d0f022e97f7c593fbce063bf22f2be94878c6c39
Wil Wilsman <wil@wilwilsman.com>
2020-07-03 10:46:30 -0500

    ✨ Add type action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 export { default as keydown } from './keydown';
 export { default as keyup } from './keyup';
 export { default as press } from './press';
+export { default as type } from './type';
---
+++ src/actions/type.js
@@ -0,0 +1,29 @@
+import { exec as press } from './press';
+import { dispatch } from '../dom';
+
+// Interactor method to add a type action to the interactor's queue which will trigger keydown,
+// input, and keyup events, for each character in the provied text string.
+export default function type(text, { range, delay, change } = {}) {
+  // normalize the range as an array
+  range = range != null && [].concat(range);
+
+  return this.exec(async function($element) {
+    // loop over each character
+    for (let i = 0, l = text.length; i < l; i++) {
+      // for the first char replace the range; otherwise insert after the previous char
+      press.call(this, $element, text[i], (
+        range && (i === 0 ? range : range[0] + i)
+      ));
+
+      // delay between presses if needed
+      if (delay && i !== text.length - 1) {
+        await new Promise(r => setTimeout(r, delay));
+      }
+    }
+
+    // trigger change events for form elements
+    if (change && (/^(input|textarea)$/i).test($element.tagName)) {
+      dispatch($element, 'change', { cancelable: false });
+    }
+  });
+}
