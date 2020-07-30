b81acb6757794e08ab61f611bc711b72ce66bb70
Wil Wilsman <wil@wilwilsman.com>
2020-07-29 23:51:28 -0500

    ✨ Add additional utils

--- src/utils.js
+++ src/utils.js
@@ -25,30 +25,35 @@ export {
   create,
   defineProperty,
   defineProperties,
+  entries,
   freeze,
   getOwnPropertyDescriptor,
+  getOwnPropertyDescriptors,
   getPrototypeOf,
   now,
   random,
   round
 };
 
+export function named(name, fn) {
+  return defineProperty(fn, 'name', { value: name });
+}
+
 // Maps an object's key value pairs to another object. Returning undefined or null from the map
 // function will omit the entry from the resulting object.
 export function map(obj, fn) {
-  return entries(obj).reduce((result, [k, v]) => {
-    let value = fn(v, k);
+  return !obj ? {} : entries(obj)
+    .reduce((result, [k, v]) => {
+      let value = fn(v, k);
 
-    return value != null
-      ? assign(result, { [k]: value })
-      : result;
-  }, create(null));
+      return value != null
+        ? assign(result, { [k]: value })
+        : result;
+    }, create(null));
 }
 
 // Recurses through an object and its prototype to gather property descriptors and map over them.
 export function mapPropertyDescriptors(obj, fn) {
-  if (!obj) return {};
-
   let descr = getOwnPropertyDescriptors(obj);
   let proto = getPrototypeOf(obj);
 
