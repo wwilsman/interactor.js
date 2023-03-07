3715be2eee7db746039121f365b916111861215a
Wil Wilsman <wil@enspire.com>
2023-03-07 17:12:59 -0600

    ♻ Move src files to module library

--- src/actions/trigger.js
+++
@@ -1,7 +0,0 @@
-import { dispatch } from '../dom';
-
-export default function trigger(event, options = {}) {
-  return this
-    .assert.exists()
-    .exec($el => dispatch($el, event, options));
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// The creators file generates static creator methods on the Interactor class
-import './creators';
-
-export { assertion } from './assert';
-export { default as when } from './when';
-export { default as InteractorError } from './error';
-export { default } from './interactor';
